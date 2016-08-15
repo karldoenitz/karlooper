ba78600221e1abdd203cd5a842e03a203dd8a6f8
lizhihao <karlvorndoenitz@gmail.com>
2016-08-15 15:23:13 +0800

    modified parse_command_line.py

--- karlooper/utils/parse_command_line.py
+++ karlooper/utils/parse_command_line.py
@@ -21,6 +21,18 @@ import sys
 from karlooper.config import set_cli_data
 
 
+message = """
+Usage: python index.py [name]=[value]
+           (to execute a python file)
+where options include:
+    help: for help
+    port: set port that the server to listen
+    is_debug: True or False, set True to enable debug
+    log_enable: True or False, set True to enable log
+    cookie: cookie's security key
+"""
+
+
 class CommandLineParser(object):
     """
 
@@ -39,6 +51,9 @@ class CommandLineParser(object):
     def parse_command_line():
         params = sys.argv[1:]
         __params_dict = dict({})
+        if "help" in params:
+            print message
+            sys.exit(0)
         for param in params:
             __params_dict[param.split("=")[0]] = param.split("=")[1]
         set_cli_data(data=__params_dict)
