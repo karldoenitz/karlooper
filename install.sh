7f3d7153d2f90c96e78b75ae7250c922ce045a62
karl <karlvorndoenitz@gmail.com>
2017-03-31 10:52:06 +0800

    add install shell

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] ; then
+    echo "develop: install for developers"
+    echo "release: install standard"
+elif [ $1 = "develop" ] ; then
+    echo "start install in develop mod..."
+    mkdir ~/karlooper
+    cp -r ./ ~/karlooper
+    cd ~/karlooper
+    python setup.py install
+    cd -
+elif [ $1 = "release" ] ; then
+    echo "start install..."
+    python setup.py install
+fi
