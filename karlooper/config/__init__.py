67a7b2bc4eab2e0904fe5a5fdfc0521cc4d93993
lizhihao <karlvorndoenitz@gmail.com>
2017-05-01 19:58:05 +0800

    add global data methods

--- karlooper/config/__init__.py
+++ karlooper/config/__init__.py
@@ -14,13 +14,14 @@ Usage
 
 
 __CLI_DATA = dict({})
+__GLOBAL_CONF_DATA = dict({})
 
 
 def set_cli_data(data):
-    """set global config data
+    """ set global config data
 
     :param data: a dict contain global config data
-    :return:
+    :return: None
 
     """
     global __CLI_DATA
@@ -31,10 +32,36 @@ def set_cli_data(data):
 
 
 def get_cli_data():
-    """get data from command line client
+    """ get data from command line client
 
     :return: None
 
     """
     global __CLI_DATA
     return __CLI_DATA
+
+
+def set_global_conf(key, data):
+    """ set global config data
+    
+    :param key: global data's key
+    :param data: global data
+    :return: None
+    
+    """
+    global __GLOBAL_CONF_DATA
+    if not data:
+        return
+    __GLOBAL_CONF_DATA[key] = data
+
+
+def get_global_conf(key):
+    """ get data from global config
+    
+    :param key: the data's key
+    :return: the data
+    
+    """
+    global __GLOBAL_CONF_DATA
+    return __GLOBAL_CONF_DATA
+
