025919e91c616f0ed6051688438193cd00d0971a
karl <karlvorndoenitz@gmail.com>
2017-06-06 11:08:20 +0800

    add base64encrypt

---
+++ karlooper/utils/base64encrypt.py
@@ -0,0 +1,58 @@
+# -*-coding:utf-8-*-
+"""
+
+security
+~~~~~~~~
+
+Use this model to encrypt string.
+
+Usage
+=====
+>>> d = Encryption()
+>>> d.input_key("123456789")
+>>> s = "/static/hello.js"
+>>> a = d.encode(s)
+>>> print a
+b14f1453ceddc91e492fbe883d552a2e
+>>> b = d.decode(a)
+>>> print b
+/static/hello.js
+
+"""
+
+import base64
+
+
+class Encryption(object):
+
+    def __init__(self):
+        self.__key = ""
+
+    def input_key(self, key):
+        """ set base key
+
+        :param key: str type, the base key
+        :return: None
+
+        """
+        self.__key = key
+
+    def encode(self, s):
+        """ encode a string
+
+        :param s: the string will be encoded
+        :return: encoded result
+
+        """
+        return base64.b64encode(s + self.__key)
+
+    def decode(self, s):
+        """ decode a string
+
+        :param s: the string will be decoded
+        :return: decoded result
+
+        """
+        decode_result = base64.b64decode(s)
+        result = decode_result[:len(decode_result)-len(self.__key)]
+        return result
