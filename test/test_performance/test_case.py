9b09e9a7aea3943942b61721209d6c3d011131aa
karl <karlvorndoenitz@gmail.com>
2017-09-01 14:40:34 +0800

    add test case

---
+++ test/test_performance/test_case.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from locust import HttpLocust, TaskSet, task
+
+
+class WebsiteTasks(TaskSet):
+    def on_start(self):
+        print "start..."
+
+    @task(1)
+    def index(self):
+        self.client.get("/hello-world")
+
+        # @task(1)
+        # def about(self):
+        #     self.client.get("/hello")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    host = "http://127.0.0.1:9988"
+    min_wait = 1000
+    max_wait = 5000
