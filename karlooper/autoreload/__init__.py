501abd6471bb630aa543c99437d854ef0e4a1ef5
karl <karlvorndoenitz@gmail.com>
2017-09-15 17:08:46 +0800

    add annotations

--- karlooper/autoreload/__init__.py
+++ karlooper/autoreload/__init__.py
@@ -1,4 +1,20 @@
 # -*-coding:utf-8-*-
+"""
+
+auto reload
+~~~~~~~~~~~
+
+Use this model to watch your project and reload your project when the code was modified.
+
+Usage
+=====
+>>> from karlooper.autoreload import AutoReload
+>>> from karlooper.web.application import Application
+>>> application = Application()
+>>> auto_reload = AutoReload(application)
+>>> auto_reload.run()
+
+"""
 
 
 class AutoReload(object):
